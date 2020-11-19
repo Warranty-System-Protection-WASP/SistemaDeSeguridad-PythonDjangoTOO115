fc8f0b9a6f234821c5461aca107969534d349d05
FreDev <fredymartinezues@gmail.com>
2020-11-18 23:36:08 -0600

    Archivos estaticos

--- WASP_TOO115/WASP_TOO115/settings.py
+++ WASP_TOO115/WASP_TOO115/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Cuenta',
-    'PuestoDT',
-    'Rol',
-    'UnidadOrganizacional',
+    'apps.Cuenta',
+    'apps.PuestoDT',
+    'apps.Rol',
+    'apps.UnidadOrganizacional',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +58,8 @@ ROOT_URLCONF = 'WASP_TOO115.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #Direcion de las templates
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,4 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#Para archivos estaticos 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ WASP_TOO115/apps/Rol/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from apps.Cuenta.models import Cuenta
+
+# Create your models here.
+class Rol(models.Model):
+    idRol=models.IntegerField(primary_key=True)
+    nombreRol=models.CharField(max_length=50)
+    descripRol=models.CharField(max_length=200)
+
+class OpcionCrud(models.Model):
+    idOpcion=models.IntegerField(primary_key=True)
+    numCrud=models.IntegerField()    
+    descripCrud=models.CharField(max_length=100)
+
+class RolOpcion(models.Model):
+    idOpcion=models.ForeignKey(OpcionCrud, on_delete=models.PROTECT)
+    idRol=models.ForeignKey(Rol, on_delete=models.PROTECT)
+
+class RolUsuario(models.Model):
+    idEmpleado=models.ForeignKey(Cuenta, on_delete=models.PROTECT)
+    idRol=models.ForeignKey(Rol, on_delete=models.PROTECT)
+    is_activo=models.BooleanField()
