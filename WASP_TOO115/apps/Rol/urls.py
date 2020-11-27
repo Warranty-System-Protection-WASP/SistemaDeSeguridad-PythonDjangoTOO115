60d5e9f7cdde3581c6fa0118d28b5a6f9ff657c2
JEfrainDR <djoseefrainr@gmail.com>
2020-11-27 16:34:23 -0600

    Vista de Lista De Control De Acceso y Update
    Solo funciona el update de la lista de control de acceso. Pero no es recomendable que lo prueben aun.

--- WASP_TOO115/apps/Rol/urls.py
+++ WASP_TOO115/apps/Rol/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 
-from apps.Rol.views import index_roles, view_rol, edit_rol, delete_rol
+from apps.Rol.views import index_roles, view_rol, edit_rol, delete_rol, update_acceso
 
 urlpatterns = [
-    path(r'^$', index_roles, name='index roles'),
-    path(r'^Nuevo$', view_rol, name='view roles'),
-    path(r'^Editar/(?P<id_rol>[^/]+)/$', edit_rol, name='edit roles'),
-    path(r'^Eliminar/(?P<id_rol>[^/]+)/$', delete_rol, name='delete roles'),
+    path('', index_roles, name='index roles'),
+    path('Nuevo', view_rol, name='view roles'),
+    path('Editar/(?P<id_rol>[^/]+)/', edit_rol, name='edit roles'),
+    path('Eliminar/(?P<id_rol>[^/]+)/', delete_rol, name='delete roles'),
+    path('Acceso', update_acceso, name='update acceso'),
 ]
--- WASP_TOO115/apps/Rol/views.py
+++ WASP_TOO115/apps/Rol/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
-from django.utils.safestring import mark_safe
 from django.core import serializers
+from django.http import HttpResponse
+from django.db import connection
+from django.core.exceptions import ObjectDoesNotExist
+
 from apps.Rol.models import Rol, RolOpcion
 from apps.Rol.forms import rol_form
 
 import json
 
 # Create your views here.
+
+switches = ["customSwitch1","customSwitch2","customSwitch3","customSwitch4","customSwitch5","customSwitch6","customSwitch7","customSwitch8","customSwitch9","customSwitch10","customSwitch11","customSwitch12"]
+
 def index_roles(request):
     rol = Rol.objects.all()
     context = {'roles':rol}
@@ -25,6 +31,8 @@ def view_rol(request):
 
 def edit_rol(request, id_rol):
     rol = Rol.objects.get(id = id_rol)
+    #En acceso se obtienen todos los permisos/accesos que se tienen en el rol especificado
+    #Luego se serializa en JSON y se envia más abajo en el diccionario
     acceso = RolOpcion.objects.filter(idRol = id_rol)
     js_data = serializers.serialize('json', acceso)
     if request.method == 'GET':
@@ -35,6 +43,38 @@ def edit_rol(request, id_rol):
             form.save()
         return redirect('index roles')
     return render(request, 'rol/form_rol.html', {'form':form, 'my_data':js_data})
+
+#Esta vista se encarga de recibir a traves de un POST la informacion del formulario de acceso
+def update_acceso(request):
+   if request.method == 'POST':
+       if 'id' in request.POST:
+           if 'estado' in request.POST:
+               contador = 1
+               for sw in switches:
+                   if request.POST['id'] == sw:
+                       try:
+                           if request.POST['estado'] == 'checked':
+                               with connection.cursor() as cursor:
+                                   cursor.execute("INSERT INTO ROL_ROLOPCION (IDOPCION_ID,IDROL_ID) VALUES ( %s , %s );", [contador,1])
+                                   cursor.close()
+                       except:
+                           obj = RolOpcion.objects.filter(idRol = 1, idOpcion = contador)
+                           #obj = obj1.filter(idOpcion = contador)
+                           if request.POST['estado'] == 'unchecked':
+                               obj.delete()
+                               #opcion = OpcionCrud.objects.get(id = contador)
+                               #rol = Rol.objects.get(id = 1)
+                               #op = RolOpcion.objects.create(idOpcion = opcion, idRol = rol)
+                       #if obj is None:
+                        #   if request.POST['estado'] == 'checked':
+                        #       op = RolOpcion.objects.create(idOpcion = contador, idRol = '1')
+                               #op.save(force_insert=True)
+                       #else:
+                        #   if request.POST['estado'] == 'unchecked':
+                        #       obj.delete()
+                       return HttpResponse('success')
+                   contador +=1
+
 
 def delete_rol(request, id_rol):
     rol = Rol.objects.get(id = id_rol)
