4e2131883cdde012d5318166991dd5a89b90d311
FreDev <fredymartinezues@gmail.com>
2020-12-03 21:29:29 -0600

    Create y List de unidad

--- WASP_TOO115/apps/UnidadOrganizacional/models.py
+++ WASP_TOO115/apps/UnidadOrganizacional/models.py
@@ -4,7 +4,7 @@ from apps.Rol.models import Rol
 
 # Create your models here.
 class UnidadOrganizacional(models.Model):
-    idUnidad=models.IntegerField(primary_key=True)
-    idRol=models.ForeignKey(Rol, on_delete=models.PROTECT)
+    idUnidad=models.AutoField(primary_key=True)
+    #idRol=models.ForeignKey(Rol, on_delete=models.PROTECT)
     nombreUnidad=models.CharField(max_length=50)
     descripUnidad=models.CharField(max_length=200)
--- WASP_TOO115/apps/UnidadOrganizacional/views.py
+++ WASP_TOO115/apps/UnidadOrganizacional/views.py
@@ -5,7 +5,7 @@ from django.db import connection
 from django.core.exceptions import ObjectDoesNotExist
 
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from apps.UnidadOrganizacional.models import UnidadOrganizacional
 from apps.UnidadOrganizacional.forms import unidad_form
@@ -21,13 +21,12 @@ class UnidadCreate(CreateView):
     model=UnidadOrganizacional
     template_name='unidad/form_unidad.html'
     form_class=unidad_form
-    success_url=reverse_lazy('UnidadOrganizacional:UnidadList') #Para que nos devuelva a la lista
-
-    #
+    success_url=reverse_lazy('index unidad') #Para que nos devuelva a la lista
+    
     def get_context_data(self, **kwargs):
         context=super(UnidadCreate, self).get_context_data(**kwargs)
         #Le mandamos el formulario en el context
-        if 'form' is not context:
+        if 'form' not in context:
             #Se agrega el formulario al contexto
             context['form']=self.form_class(self.request.GET)
         return context
@@ -45,70 +44,18 @@ class UnidadCreate(CreateView):
         else:
             return self.render_to_response(self.get_context_data(form=form))
 
+class UnidadUpdate(UpdateView):
+    model=UnidadOrganizacional
+    template_name='unidad/form_unidad.html'
+    form_class=unidad_form
+    
 
-
-
-
-# # Create your views here.
-# def index_unidad(request):
-#     #Traemos todos los objetos
-#     unidad=UnidadOrganizacional.objects.all()
-#     context={'unidades':unidad}
-#     #Retornamos a la vista
-#     return render(request,'unidad/index_unidad.html',context)
-
-# def view_unidad(request):
-#     js_data=""
-#     if request.method=='POST':
-#         form=unidad_form(request.POST)
-#         if form.is_valid():
-#             form.save()
-#         return redirect('index unidad')
-#     else:
-#         form=unidad_form()    
-#     return render(request, 'unidad/form_unidad.html',{'form':form, 'my_data':js_data})
-
-
-# #Para editar la unidad necesitamos el id de la unidad
-# def edit_unidad(request, id_unidad):
-#     unidad=UnidadOrganizacional.objects.get(id= id_unidad)
-#     acceso=UnidadOrganizacional.objects.filter(idUnidad=id_unidad)
-#     js_data=serializers.serialize('json',acceso)
-#     if request.method=='GET':
-#         form=unidad_form(instance=rol)
-#     else:
-#         form=unidad_form(request.POST, instace=unidad)
-#         if form.is_valid():
-#             form.save()
-#         return redirect('index unidad')
-#     return request(request,'unidad/unidad_unidad.html',{'form':form, 'my_data':js_data})
-
-# def delete_unidad(request, id_unidad):
-#     return request('unidad/delete_unidad.html')
-
-
-# def create_acceso(request):
-#     if request.method == 'GET':
-#         return render(request, 'unidad/acceso_unidad.html')
-
-#     if request.method == 'POST':
-#         if 'idunidad' in request.POST:
-#             if 'estado' in request.POST:
-#                 contador = 1
-#                 for sw in switches:
-#                     if request.POST['id'] == sw:
-#                         try:
-#                             obj = UnidadOrganizacional.objects.filter( idUnidad= 1, idOpcion = contador)
-#                             if request.POST['estado'] == 'unchecked':
-#                                 obj.delete()
-#                             if request.POST['estado'] == 'checked':
-#                                 with connection.cursor() as cursor:
-#                                     #Query para insertar una nueva unidad organizacional
-#                                     cursor.execute("INSERT INTO UNIDADORGANIZACIONAL_UNIDA4EEA (NOMBREUNIDAD,DESCRIPUNIDAD,IDROL_ID) VALUES ( %s, %s, %s );",[contador,1])
-#                                     cursor.close()
-#                         except:
-#                             obj = UnidadOrganizacional.objects.filter(idRol = 1, idOpcion = contador)
-#                             if request.POST['estado'] == 'unchecked':
-#                                 obj.delete()
-#                         return HttpResponse('success')
-#                     contador +=1
+    def get_context_data(self, **kwargs):
+        #Aca llamamos a los objetos y que se rendericen en cada texto de nuestro update
+        context=super(UnidadUpdate, self).get_context_data(**kwargs)
+        pk=self.kwargs
+        solicitud=self.model.objets.get(id=pk)
+        if  'form' not in context:
+            context['form']=self.form_class()
+        context['id']=pk
+        return context
