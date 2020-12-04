08ddb8f95f8cc179b341923effbf014bc4a9a6a1
abcrz <>
2020-12-04 04:09:16 -0600

    Validación de contraseña
    Debe tener una lóngitud mínima de 12 cáracteres de los cuales por lo menos 1 debe ser caracter especial, al menos 1 debe ser letra mínuscula, al menos 1 debe ser letra mayúscula y al menos 1 debe ser número

--- WASP_TOO115/WASP_TOO115/settings.py
+++ WASP_TOO115/WASP_TOO115/settings.py
@@ -96,18 +96,10 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+ #Para validar forma de contraseña
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'apps.Cuenta.validacion_contrasenia.ValidadorPersonalizado',
     },
 ]
 
--- WASP_TOO115/apps/Cuenta/views.py
+++ WASP_TOO115/apps/Cuenta/views.py
@@ -233,7 +233,7 @@ class PasswordChangeView(FormView):
 
     def form_valid(self, form):
         form.save()
-        # Updating the password logs out all other sessions for the user
-        # except the current one.
+        # La actualización de la contraseña cierra todas las demás sesiones del usuario.
+        # excepto el actual.
         update_session_auth_hash(self.request, form.user)
         return super().form_valid(form)
